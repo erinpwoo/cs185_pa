39e8340e1ec833eaefe2ffcd3128937a2607a74d
Erin <erinpwoo@yahoo.com>
2021-01-27 17:40:05 -0800

    Added email form validation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var modal = document.getElementById("modal-window")
 var img = document.getElementById("modal-img")
 var scrollToTopButton = document.getElementById("scrollToTopBtn");
+var submitMsg = document.getElementById("submitMsg");
 var root = document.documentElement;
 
 function openModal(source) {
@@ -26,6 +27,16 @@ document.addEventListener("scroll", () => {
         scrollToTopButton.classList.remove("showButton");
     }
 });
+
+function validateEmail(email) {
+    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    submitMsg.style.visibility = "visible";
+    if (email.match(regex)) {
+        submitMsg.textContent = "Email successfully recorded."
+    } else {
+        submitMsg.textContent = "Invalid email address."
+    }
+}
 
 scrollToTopButton.addEventListener("click", () => {
     root.scrollTo({
